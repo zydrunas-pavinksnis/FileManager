ea8244c7501f03efc0a72e26b74511dd5935fea6
smallZydrius <zydrunas.pavinksnis@gmailcom>
2020-07-24 23:01:50 +0300

    css

--- action.php
+++ action.php
@@ -4,6 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File viewer</title>
+    <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
 </head>
 <body>
     <?php
